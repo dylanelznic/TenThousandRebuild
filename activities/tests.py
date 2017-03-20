a31857e3a569e976e78d47041a3efe9f20171411
Dylan Elznic <djey96@mst.edu>
2017-03-19 23:14:55 -0500

    [UPDATE] Redriect after POST, display last text input

--- activities/tests.py
+++ activities/tests.py
@@ -20,8 +20,20 @@ class HomePageTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new activity item'})
-		self.assertIn('A new activity item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new activity item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/', data={'item_text': 'A new activity item'})
+		
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_only_saves_items_when_necessary(self):
+		self.client.get('/')
+		self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
@@ -30,7 +42,8 @@ class ItemModelTest(TestCase):
 		new_item.text = 'The new item'
 		new_item.save()
 
-		saved_item = Item.objects.all()
-		self.assertEqual(saved_item.count(), 1)
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 1)
 
-		self.assertEqual(saved_item.text, 'The new item')
+		only_item = saved_items[0]
+		self.assertEqual(only_item.text, 'The new item')
